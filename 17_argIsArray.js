2d27fdaa2e8072c5db7b614a0d0ed5388fea1240
AlexKVal <alexkval@gmail.com>
2015-03-22 19:09:32 +0200

    17_argIsArray

--- 17_argIsArray.js
+++ 17_argIsArray.js
@@ -5,3 +5,10 @@ function isArray(obj) {
 console.log('[1,2,3] ' + isArray([1,2,3]));
 console.log('{a:1, b:2} ' + isArray({a:1, b:2}));
 console.log('1,2,3 ' + isArray(1,2,3));
+
+console.log('\n\n--------------------------');
+console.log('or use new Array.isArray()');
+
+console.log('[1,2,3] ' + Array.isArray([1,2,3]));
+console.log('{a:1, b:2} ' + Array.isArray({a:1, b:2}));
+console.log('1,2,3 ' + Array.isArray(1,2,3));
